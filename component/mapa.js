02547b460296a1308fc5c4527102c1a2f3e1e9d4
first29 <chumpitaz49@gmail.com>
2023-10-16 19:33:49 -0500

    mejora inicio

--- component/mapa.js
+++ component/mapa.js
@@ -8,39 +8,39 @@ import { useRoute } from '@react-navigation/native';
 const Mapa = () => {
     route = useRoute();
     const { task, userLocation } = route.params;
-    const tlat = task.location.latitude;
-    const tlon = task.location.longitude;
-    console.log(task.location, userLocation);
+    const tlat = task.mapa.latitude;
+    const tlon = task.mapa.longitude;
+    console.log(task.mapa, userLocation);
     const distancia = (x,y,x1,y1,radio) => {
         const dist = Math.sqrt(Math.pow(x - x1, 2) + Math.pow(y - y1, 2));
         const veracidad = dist <= radio
         if (veracidad) return true;
         return false;
     };
-    let color=distancia(userLocation.latitude,userLocation.longitude,tlat,tlon,task.radius) ? 'rgba(0, 128, 0, 0.3)' : 'rgba(255, 0, 0, 0.5)'
-    let color_texto=distancia(userLocation.latitude,userLocation.longitude,tlat,tlon,task.radius) ? 'rgba(0, 128, 0, 1)' : 'rgba(255, 0, 0, 1)'
+    let color=distancia(userLocation.latitude,userLocation.longitude,tlat,tlon,task.radio) ? 'rgba(0, 128, 0, 0.3)' : 'rgba(255, 0, 0, 0.5)'
+    let color_texto=distancia(userLocation.latitude,userLocation.longitude,tlat,tlon,task.radio) ? 'rgba(0, 128, 0, 1)' : 'rgba(255, 0, 0, 1)'
     return (
         <View style={styles.container}>
             <MapView style={styles.map}
                 initialRegion={{
-                    latitude: task.location.latitude,
-                    longitude: task.location.longitude,
+                    latitude: tlat,
+                    longitude: tlon,
                     latitudeDelta: 0.001,
                     longitudeDelta: 0.001,
                 }}
             >
                 <Marker coordinate={userLocation} title="Tu ubicación" />
                 <>
-                    <Marker coordinate={task.location} title="tarea" />
+                    <Marker coordinate={task.mapa} title="tarea" pinColor="#FFC000"/>
                     <Circle
-                        center={task.location}
-                        radius={task.radius}
+                        center={task.mapa}
+                        radius={task.radio}
                         fillColor={color}
                     />
                 </>
             </MapView>
             <Text style={{ position: 'absolute', bottom: 16, alignSelf: 'center', fontSize: 30, color: color_texto}}>
-                {distancia(userLocation.latitude,userLocation.longitude,tlat,tlon,task.radius) ? 'Dentro del área' : 'Fuera del área'}
+                {distancia(userLocation.latitude,userLocation.longitude,tlat,tlon,task.radio) ? 'Dentro del área' : 'Fuera del área'}
             </Text>
         </View>
     );
--- component/taskdata.js
+++ component/taskdata.js
@@ -1,14 +1,17 @@
 const tasksData = [
     {
-        'id': "0",
-        'title': "Tarea 1",
-        'location': { latitude: -12.061036, longitude: -77.105172 },
-        'radius': 25,
+        'IdTarea': 0,
+        'Nombre_Tarea': "Tarea 1",
+        'Prioridad': 3,
+        'IdResponsable': 1,
+        'IdProyecto': 1,
+        'mapa': { latitude: -12.061036, longitude: -77.105172 },
+        'radio': 25,
         'phases': [
-          { id: "1", title: "Fase 1", status: "iniciado", startTime: null, endTime: null },
-          { id: "2", title: "Fase 2", status: "pendiente", startTime: null, endTime: null },
-          { id: "3", title: "Fase 3", status: "pendiente", startTime: null, endTime: null },
-          { id: "4", title: "Fase 4", status: "pendiente", startTime: null, endTime: null },
+          { id: 1, title: "Fase 1", status: "iniciado", startTime: null, endTime: null },
+          { id: 2, title: "Fase 2", status: "pendiente", startTime: null, endTime: null },
+          { id: 3, title: "Fase 3", status: "pendiente", startTime: null, endTime: null },
+          { id: 4, title: "Fase 4", status: "pendiente", startTime: null, endTime: null },
         ],
       },
       {
