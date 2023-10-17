ef7452254950b6435a4f005fe1afa839a7426ed4
first29 <chumpitaz49@gmail.com>
2023-10-16 19:34:09 -0500

    Mejora

--- component/inicio.js
+++ component/inicio.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, TouchableOpacity, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import * as Location from 'expo-location';
 import { useState, useEffect } from 'react';
 import tasksData from './taskdata';
@@ -15,21 +15,51 @@ export default function Inicio({ navigation }) {
             }
             let location = await Location.getCurrentPositionAsync({});
             setUserLocation({ latitude: location.coords.latitude, longitude: location.coords.longitude });
-            
+
         })();
-    },[]);
+    }, []);
     console.log(userLocation);
-    const handleViewOnMap = (userLocation) => {
-        const  task=tasksData[0]
-        navigation.navigate('Map', { userLocation,task });
+    const handleViewOnMap = (userLocation, mapa,radio) => {
+        navigation.navigate('Map', { userLocation, mapa,radio });
     };
+
     return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <TouchableOpacity onPress={() => handleViewOnMap(userLocation)}>
-                <Text style={styles.actionButton}>Ver en el mapa</Text>
-            </TouchableOpacity>
-        </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView >
+                <View style={styles.container}>
+                    {tasksData.map(task => {
+                        let faseAnteriorTerminada = true;  // Variable para verificar si la fase anterior está "terminado"
+                        return (
+                            <View key={task.IdTarea} style={styles.taskContainer}>
+                                <Text>Nombre de la tarea: {task.Nombre_Tarea}</Text>
+                                <Text>Prioridad: {task.Prioridad}</Text>
+                                <Text>Responsable: {task.IdResponsable}</Text>
+                                <Text>Proyecto: {task.IdProyecto}</Text>
+                                <Text>Fases:</Text>
+                                {task.phases.map((phase, index) => {
+                                    if(index===0) faseAnteriorTerminada=true;
+                                    else  faseAnteriorTerminada = task.phases[index - 1].status === 'terminado';
+                                    // Muestra la fase si está "pendiente" y la fase anterior está "terminado"
+                                    if ((phase.status == 'pendiente'||'iniciado') && faseAnteriorTerminada) {
+                                        return (
+                                            <View key={phase.id} style={styles.phaseContainer}>
+                                                <Text>Título de Fase: {phase.title}</Text>
+                                                <Text>Estado: {phase.status}</Text>
+                                            </View>
+                                        );
+                                    }
+                                    return null;  // Si no se cumple la condición, no muestra la fase
+                                })}
+                                <TouchableOpacity onPress={() => handleViewOnMap(userLocation,task.mapa,task.radio)}>
+                                    <Text style={styles.actionButton}>Ver en el mapa</Text>
+                                </TouchableOpacity>
+                            </View>
+                        );
+                    })}
+                </View>
+            </ScrollView >
+
+        </SafeAreaView >
     );
 }
 
@@ -39,5 +69,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    taskContainer: {
+        marginBottom: 20,
+    },
+    phaseContainer: {
+        marginLeft: 20,
     },
 });
--- component/mapa.js
+++ component/mapa.js
@@ -7,40 +7,47 @@ import { useRoute } from '@react-navigation/native';
 
 const Mapa = () => {
     route = useRoute();
-    const { task, userLocation } = route.params;
-    const tlat = task.mapa.latitude;
-    const tlon = task.mapa.longitude;
-    console.log(task.mapa, userLocation);
+    console.log(route.params);
+    const { mapa,radio, userLocation } = route.params;
+    console.log(mapa,radio, userLocation);
     const distancia = (x,y,x1,y1,radio) => {
-        const dist = Math.sqrt(Math.pow(x - x1, 2) + Math.pow(y - y1, 2));
+        console.log(x,y,x1,y1,radio);
+        const X = (x * Math.PI) / 180;
+        const Y = (y * Math.PI) / 180;
+        const X1 = (x1 * Math.PI) / 180;
+        const Y1 = (y1 * Math.PI) / 180;
+        const radioTierra = 6371000;
+        const dist = (Math.sqrt(Math.pow(X - X1, 2) + Math.pow(Y - Y1, 2)))*radioTierra;
+        console.log(dist);
         const veracidad = dist <= radio
+        console.log(veracidad);
         if (veracidad) return true;
         return false;
     };
-    let color=distancia(userLocation.latitude,userLocation.longitude,tlat,tlon,task.radio) ? 'rgba(0, 128, 0, 0.3)' : 'rgba(255, 0, 0, 0.5)'
-    let color_texto=distancia(userLocation.latitude,userLocation.longitude,tlat,tlon,task.radio) ? 'rgba(0, 128, 0, 1)' : 'rgba(255, 0, 0, 1)'
+    const color=distancia(userLocation.latitude,userLocation.longitude,mapa.latitude,mapa.longitude,radio) ? 'rgba(0, 128, 0, 0.3)' : 'rgba(255, 0, 0, 0.5)'
+    const color_texto=distancia(userLocation.latitude,userLocation.longitude,mapa.latitude,mapa.longitude,radio) ? 'rgba(0, 128, 0, 1)' : 'rgba(255, 0, 0, 1)'
     return (
         <View style={styles.container}>
             <MapView style={styles.map}
                 initialRegion={{
-                    latitude: tlat,
-                    longitude: tlon,
+                    latitude: mapa.latitude,
+                    longitude: mapa.longitude,
                     latitudeDelta: 0.001,
                     longitudeDelta: 0.001,
                 }}
             >
                 <Marker coordinate={userLocation} title="Tu ubicación" />
                 <>
-                    <Marker coordinate={task.mapa} title="tarea" pinColor="#FFC000"/>
+                    <Marker coordinate={mapa} title="tarea" pinColor="#FFC000"/>
                     <Circle
-                        center={task.mapa}
-                        radius={task.radio}
+                        center={mapa}
+                        radius={radio}
                         fillColor={color}
                     />
                 </>
             </MapView>
             <Text style={{ position: 'absolute', bottom: 16, alignSelf: 'center', fontSize: 30, color: color_texto}}>
-                {distancia(userLocation.latitude,userLocation.longitude,tlat,tlon,task.radio) ? 'Dentro del área' : 'Fuera del área'}
+                {distancia(userLocation.latitude,userLocation.longitude,mapa.latitude,mapa.longitude,radio) ? 'Dentro del área' : 'Fuera del área'}
             </Text>
         </View>
     );
@@ -56,6 +63,6 @@ const styles = StyleSheet.create({
     map: {
         width: '100%',
         height: '100%',
-    }
+    },
 });
 export default Mapa;
